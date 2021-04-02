06ff70fa7ef68d11942eb1d5a0af3dcb7536c4c4
SrAllison52 <78006833+allison5200a@users.noreply.github.com>
2021-04-02 17:47:40 -0300

    Update numowner.js

--- numowner.js
+++ numowner.js
@@ -1,10 +1,10 @@
-const ownerNumber = ["557187645787@s.whatsapp.net"]
-const OriginalOwner = '557187645787'
+const ownerNumber = ["558688036194@s.whatsapp.net"]
+const OriginalOwner = '558688036194'
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Meu criador^~^\n' 
+            + 'FN:Meu Dono^~^\n' 
             + 'ORG:Ian;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=557187645787:+55 71 8764-5787\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=558688036194:+55 86 8803-6194\n' 
             + 'END:VCARD'
 
 exports.vcard = vcard
